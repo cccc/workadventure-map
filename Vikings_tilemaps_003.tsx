6a3dd5db52b5d9d9e5fbeb2f939353ebee4ff380
Jochim Selzer <jrsusenet-130627@yahoo.de>
2021-04-04 21:51:57 +0200

    grass

---
+++ Vikings_tilemaps_003.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Vikings_tilemaps_003" tilewidth="32" tileheight="32" tilecount="60" columns="10">
+ <image source="../Downloads/Vikings_tilemaps_003.bmp" width="320" height="200"/>
+</tileset>
