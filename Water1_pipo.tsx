993aebc7a84afae85d51104eade85b458167e51d
Jochim Selzer <jrsusenet-130627@yahoo.de>
2021-04-05 10:48:57 +0200

    strange file names

---
+++ Water1_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Water1_pipo" tilewidth="32" tileheight="32" tilecount="40" columns="8">
+ <image source="Water1_pipo.png" width="256" height="160"/>
+</tileset>
