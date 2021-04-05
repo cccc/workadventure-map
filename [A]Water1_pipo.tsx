cb03fdfca0db01fa1e74ed81ed24be8a4feeb74b
Jochim Selzer <jrsusenet-130627@yahoo.de>
2021-04-05 12:04:38 +0200

    renamed files

--- [A]Water1_pipo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="[A]Water1_pipo" tilewidth="32" tileheight="32" tilecount="40" columns="8">
- <image source="[A]Water1_pipo.png" width="256" height="160"/>
-</tileset>
